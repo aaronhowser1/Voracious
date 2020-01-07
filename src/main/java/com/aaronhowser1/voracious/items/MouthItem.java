113d0998a39f05bb9d5104c1a97f9197bd77ac8b
Aaron Howser <aaronhowser1@gmail.com>
2020-01-07 16:40:48 -0600

    Apparently better, but I don't know why Also, it gives infinite food and saturation if it has any tag, including being renamed

--- src/main/java/com/aaronhowser1/voracious/items/MouthItem.java
+++ src/main/java/com/aaronhowser1/voracious/items/MouthItem.java
@@ -1,5 +1,7 @@
 package com.aaronhowser1.voracious.items;
 
+import com.google.common.collect.Lists;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -7,9 +9,19 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.FloatNBT;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class MouthItem extends Item {
 
@@ -46,7 +58,8 @@ public class MouthItem extends Item {
             float hp = stack.getTag().getFloat("stored_hp");
             float a = Math.max(Math.min(hp, 1F), (20 - efl));
             stack.setTagInfo("stored_hp", FloatNBT.func_229689_a_(hp - a));
-            ((ServerPlayerEntity) entity).getFoodStats().setFoodSaturationLevel(Math.round(efl + a));
+            ((ServerPlayerEntity) entity).getFoodStats().addStats(1, 1F);
+//            ((ServerPlayerEntity) entity).getFoodStats().setFoodSaturationLevel(Math.round(efl + a));
         }
     }
 }
